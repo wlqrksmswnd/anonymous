caa513e8a5b895843623aa1da92b804697db799d
mingi0317 <hotcream0000@gmail.com>
2025-05-29 01:24:25 +0900

    상단 바 UI구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
-
+import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+
+
   return (
     <>
+      <div id='everything'>
+        <div id='top-bar'>
+              <h2 id='title'>TAPIE Board</h2>
+              <div id='login'>
+                <p id='name'>{name}</p> {/*로그인시 닉네임 값 가져오기기*/ }
+                <button id='login-button'><FontAwesomeIcon icon={faRightToBracket} id='login-icon' />로그인</button>
+              </div>
+        </div>
+        <div id='main'>
 
+        </div>
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
